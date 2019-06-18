62de4cd895df89ef1c89b2236e1e66ee92698eca
Erik J Brown <ebrown0989@gmail.com>
2019-06-18 17:23:45 -0500

    made request from react to the database

--- my_awesome_project/client/src/App.js
+++ my_awesome_project/client/src/App.js
@@ -9,11 +9,21 @@ class App extends Component {
   callAPI() {
     fetch(local.config.api_config + "/testAPI")
       .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
+
+  // Go to API and check testDB route for a response
+  callDB() {
+    fetch("http://localhost:9000/testDB")
+      .then(res => res.text())
+      .then(res => this.setState({ dbResponse: res }))
+      .catch(err => err);
   }
 
   componentDidMount() {
     this.callAPI();
+    this.callDB();
   }
 
   //componentWillUnmount() {}
@@ -23,6 +33,7 @@ class App extends Component {
       <div>
         <h1>Testing API Connection...</h1>
         <p><b>API RESPONSE =</b> {this.state.apiResponse}</p>
+        <p><b>DB RESPONE =</b> {this.state.dbResponse}</p>
       </div>
     );
   }
