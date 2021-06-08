27f5e655b9a70def91a03cbbf22ea8936811cfb0
Erik J Brown <erkjbro@gmail.com>
2021-06-08 11:25:06 -0500

    rest middleware

---
+++ typescript-apps/toptal-rest-api/users/middleware/users.middleware.ts
@@ -0,0 +1,84 @@
+import express from 'express';
+import debug from 'debug';
+
+import userService from '../services/users.service';
+
+const log: debug.IDebugger = debug('app:users-controller');
+
+class UsersMiddleware {
+  async validateRequiredUserBodyFields(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (req.body && req.body.email && req.body.password) {
+      next();
+    } else {
+      res.status(400).send({
+        error: `Missing required fields email and password`,
+      });
+    }
+  }
+
+  async validatesSameEmailDoesntExist(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const user = await userService.getUserByEmail(req.body.email);
+
+    user
+      ? res.status(400).send({ error: `User email already exists` })
+      : next();
+  }
+
+  async validateSameEmailBelongToSameUser(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const user = await userService.getUserByEmail(req.body.email);
+
+    user && user.id === req.params.userId
+      ? next()
+      : res.status(400).send({ error: `Invalid email` });
+  }
+
+  validatePatchEmail = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    if (req.body.email) {
+      log('Validating email', req.body.email);
+      this.validateSameEmailBelongToSameUser(req, res, next);
+    } else {
+      next();
+    }
+  };
+
+  async validateUserExists(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const user = await userService.readById(req.params.userId);
+
+    user
+      ? next()
+      : res.status(404).send({
+          error: `User ${req.params.userId} not found`,
+        });
+  }
+
+  async extractUserId(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    req.body.id = req.params.userId;
+    next();
+  }
+}
+
+export default new UsersMiddleware();
