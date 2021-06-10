3761d930b90bd0c55c3a32337c194b8e3e6ba5ba
Erik J Brown <erkjbro@gmail.com>
2021-06-10 15:02:07 -0500

    jwt refresh route

--- typescript-apps/toptal-rest-api/auth/auth.routes.config.ts
+++ typescript-apps/toptal-rest-api/auth/auth.routes.config.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 
 import authController from './controllers/auth.controller';
 import authMiddleware from './middleware/auth.middleware';
+import jwtMiddleware from './middleware/jwt.middleware';
 import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 import { CommonRoutesConfig } from '../common/common.routes.config';
 
@@ -18,6 +19,13 @@ class AuthRoutes extends CommonRoutesConfig {
 
       BodyValidationMiddleware.verifyBodyFieldsErrors,
       authMiddleware.verifyUserPassword,
+      authController.createJWT,
+    ]);
+
+    this.app.post(`/auth/refresh-token`, [
+      jwtMiddleware.validJWTNeeded,
+      jwtMiddleware.verifyRefreshBodyField,
+      jwtMiddleware.validRefreshNeeded,
       authController.createJWT,
     ]);
 
